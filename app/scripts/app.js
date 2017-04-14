a4ff10132e9669a9c55c15e4422b34ce5c2cd511
yuzhen <yzhen01.zy@gmail.com>
2017-04-14 21:32:43 +0800

    map-start

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,16 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json')
+    .then(rq => {
+      addSearchHeader(rq.query);
+      rq.results.map(url => {
+        getJSON(url).then(createPlanetThumb);
+      });
+    })
+    .catch(function(e){
+      addSearchHeader('unknown');
+      console.log(e);
+    });
   });
 })(document);
